24dee2e36eddddc3b541513ab375df03ba42ad84
Your Name~ <you@example.com>
2022-12-30 21:56:52 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,69 @@
+var elDropTop = document.querySelector(".content__top");
+var elTextWrapper = document.querySelector(".content__bottom");
+var elDropIcon = document.querySelector(".content__img")
+
+elDropTop.addEventListener("click", function(){
+    elTextWrapper.classList.toggle("show-text");
+    elDropIcon.classList.toggle("turn")
+    elTextWrapper2.classList.remove("show-text");
+    elDropIcon2.classList.remove("turn")
+})
+
+var elDropTop2 = document.querySelector(".content__top2");
+var elTextWrapper2 = document.querySelector(".content__bottom2");
+var elDropIcon2 = document.querySelector(".content__img2")
+
+elDropTop2.addEventListener("click", function(){
+    elTextWrapper2.classList.toggle("show-text");
+    elDropIcon2.classList.toggle("turn")
+    elTextWrapper.classList.remove("show-text");
+    elDropIcon.classList.remove("turn")
+    elTextWrapper3.classList.remove("show-text");
+    elDropIcon3.classList.remove("turn")
+})
+
+var elDropTop3 = document.querySelector(".content__top3");
+var elTextWrapper3 = document.querySelector(".content__bottom3");
+var elDropIcon3 = document.querySelector(".content__img3")
+
+elDropTop3.addEventListener("click", function(){
+    elTextWrapper3.classList.toggle("show-text");
+    elDropIcon3.classList.toggle("turn")
+    elTextWrapper2.classList.remove("show-text");
+    elDropIcon2.classList.remove("turn")
+})
+
+var elDropTop4 = document.querySelector(".content__top4");
+var elTextWrapper4 = document.querySelector(".content__bottom4");
+var elDropIcon4 = document.querySelector(".content__img4")
+
+elDropTop4.addEventListener("click", function(){
+    elTextWrapper4.classList.toggle("show-text");
+    elDropIcon4.classList.toggle("turn")
+    elTextWrapper5.classList.remove("show-text");
+    elDropIcon5.classList.remove("turn")
+})
+
+var elDropTop5 = document.querySelector(".content__top5");
+var elTextWrapper5 = document.querySelector(".content__bottom5");
+var elDropIcon5 = document.querySelector(".content__img5")
+
+elDropTop5.addEventListener("click", function(){
+    elTextWrapper5.classList.toggle("show-text");
+    elDropIcon5.classList.toggle("turn")
+    elTextWrapper4.classList.remove("show-text");
+    elDropIcon4.classList.remove("turn")
+    elTextWrapper6.classList.remove("show-text");
+    elDropIcon6.classList.remove("turn")
+})
+
+var elDropTop6 = document.querySelector(".content__top6");
+var elTextWrapper6 = document.querySelector(".content__bottom6");
+var elDropIcon6 = document.querySelector(".content__img6")
+
+elDropTop6.addEventListener("click", function(){
+    elTextWrapper6.classList.toggle("show-text");
+    elDropIcon6.classList.toggle("turn")
+    elTextWrapper5.classList.remove("show-text");
+    elDropIcon5.classList.remove("turn")
+})
